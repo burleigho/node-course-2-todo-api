a9b660ebfca5b0e23e45859ac463d71a93028698
Oliver <oliver.burleigh@atos.net>
2018-08-31 08:04:43 +0100

    add DELETE /todos/:id

---
+++ playground/mongoose-remove.js
@@ -0,0 +1,16 @@
+const { Todo } = require('../server/models/todo');
+const { User } = require('../server/models/users');
+const { mongoose } = require('../server/db/mongoose');
+const {ObjectID } = require('mongodb');
+
+// Todo.remove({}).then((result) => {
+//   console.log(result);
+// });
+
+Todo.findByIdAndRemove('5b88e4054c56e77dc55f8dde').then((todo) => {
+  console.log(todo);
+});
+
+Todo.findOneAndRemove({_id:'5b88e4054c56e77dc55f8dde'}).then((todo) => {
+  console.log(todo);
+});
